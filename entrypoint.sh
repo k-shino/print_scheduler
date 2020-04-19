278f7e6f62f2e678e36206839245fad8dc10db7d
Kenta Shinohara <kenta.shinohara.hy@hco.ntt.co.jp>
2020-04-19 12:58:34 +0900

    Remove json files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,5 @@ yq r config.yml -j > config.json
 yq r env-definition.yml -j > env-definition.json
 yq r tasks.yml -j > tasks.json
 python /print/print_schedule.py
+rm config.json
+rm env-definition.json
